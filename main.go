16115b51bda5f74b79ffde174712e6550ca652d4
wrkode <william.rizzo@suse.com>
2023-09-23 10:34:55 +0200

    optimize for sustainability with go routines and regex pre-compile

--- main.go
+++ main.go
@@ -3,74 +3,103 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 	"regexp"
+	"sync"
 )
 
-var keywords = []string{"carbon", "climate", "energy", "green", "kepler", "sustainability", "sustainable"}
-
-func main() {
-	// Read URLs from urls.txt file into a slice
-	file, err := os.Open("urls.txt")
+func readLinesFromFile(filename string) ([]string, error) {
+	file, err := os.Open(filename)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer file.Close()
 
-	var urls []string
+	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		urls = append(urls, scanner.Text())
+		lines = append(lines, scanner.Text())
 	}
 
-	// Loop through the list of URLs
-	for _, url := range urls {
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Printf("Failed to fetch URL %s: %v\n", url, err)
-			continue
-		}
+	return lines, scanner.Err()
+}
 
-		contentBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Printf("Failed to read response body from URL %s: %v\n", url, err)
-			continue
-		}
-		content := string(contentBytes)
-		resp.Body.Close()
+func processURL(url string, keywordRegexes []*regexp.Regexp, titleRegex *regexp.Regexp, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-		var talks []string
-		encounteredTitles := make(map[string]bool)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Failed to fetch URL %s: %v\n", url, err)
+		return
+	}
+	defer resp.Body.Close()
 
-		// Loop through the list of keywords
-		for _, keyword := range keywords {
-			regex := regexp.MustCompile(keyword)
-			matches := regex.FindAllIndex([]byte(content), -1)
+	contentBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Failed to read response body from URL %s: %v\n", url, err)
+		return
+	}
+	content := string(contentBytes)
 
-			for _, match := range matches {
-				// Extract the title from the matched content
-				titleRegex := regexp.MustCompile(".*'>(.*?)<span class=\"vs\">.*")
-				titleMatch := titleRegex.FindStringSubmatch(content[match[0]:])
-				if len(titleMatch) > 1 {
-					title := titleMatch[1]
+	var talks []string
+	encounteredTitles := make(map[string]bool)
 
-					if len(title) >= 40 && !encounteredTitles[title] {
-						encounteredTitles[title] = true
-						talks = append(talks, "- "+title)
-					}
+	for _, regex := range keywordRegexes {
+		matches := regex.FindAllIndex([]byte(content), -1)
+
+		for _, match := range matches {
+			titleMatch := titleRegex.FindStringSubmatch(content[match[0]:])
+			if len(titleMatch) > 1 {
+				title := titleMatch[1]
+
+				if len(title) >= 40 && !encounteredTitles[title] {
+					encounteredTitles[title] = true
+					talks = append(talks, "- "+title)
 				}
 			}
 		}
+	}
 
-		// Print the conference schedule link and "Talks:" section if talks were found
-		if len(talks) > 0 {
-			fmt.Println("Conference schedule link:", url)
-			fmt.Println("Talks:")
-			for _, talk := range talks {
-				fmt.Println(talk)
-			}
+	if len(talks) > 0 {
+		fmt.Println("Conference schedule link:", url)
+		fmt.Println("Talks:")
+		for _, talk := range talks {
+			fmt.Println(talk)
 		}
 	}
+}
+
+func main() {
+	keywords, err := readLinesFromFile("keywords.txt")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to read keywords from file: %v", err))
+	}
+
+	urls, err := readLinesFromFile("urls.txt")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to read URLs from file: %v", err))
+	}
+
+	titleRegex := regexp.MustCompile(".*'>(.*?)<span class=\"vs\">.*")
+	keywordRegexes := make([]*regexp.Regexp, len(keywords))
+	for i, keyword := range keywords {
+		keywordRegexes[i] = regexp.MustCompile(keyword)
+	}
+
+	const concurrentLimit = 5
+	sem := make(chan struct{}, concurrentLimit) // semaphore pattern for limiting concurrency
+	var wg sync.WaitGroup
+
+	for _, url := range urls {
+		wg.Add(1)
+		sem <- struct{}{} // acquire a token
+		go func(u string) {
+			processURL(u, keywordRegexes, titleRegex, &wg)
+			<-sem // release a token
+		}(url)
+	}
+
+	wg.Wait()
 }
